3dd5f9ed9f8d8895022672d03de47e9f5736d897
Marco Montilla <marco.montilla@telrite.com>
2024-06-20 09:03:55 -0400

    217. Reading and Setting a Query String

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
-import styles from './Map.module.css'
+import React from "react";
+import styles from "./Map.module.css";
+import { useSearchParams } from "react-router-dom";
 
 function Map() {
+  const [searchParams, setSearch] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
   return (
     <div className={styles.mapContainer}>
-        Map
+      <h1>Map</h1>
+      <h1>
+        Position: {lat}, {lng}
+      </h1>
     </div>
-  )
+  );
 }
 
-export default Map
+export default Map;
