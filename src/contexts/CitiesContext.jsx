92cb2477d75a504d4425e97a0b9d56e14bb91d8d
Marco Montilla <marco.montilla@telrite.com>
2024-06-25 12:03:53 -0400

    227. Creating a CitiesContext

---
+++ src/contexts/CitiesContext.jsx
@@ -0,0 +1,39 @@
+import { createContext, useEffect, useState } from "react";
+
+const CitiesContext = createContext;
+
+const BASE_URL = "http://localhost:9000";
+
+function CitiesProvider({ children }) {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchCitites() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("Ops, something when wrong...");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCitites();
+  }, []);
+
+  return (
+    <CitiesContext.Provider
+      value={{
+        cities,
+        isLoading,
+      }}
+    >
+      {children}
+    </CitiesContext.Provider>
+  );
+}
+
+export { CitiesProvider };
